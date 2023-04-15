4c9318fbb87d9a03e0acd1a6a8f698ab1edab1f6
1amSumit <sj779619@gmail.com>
2023-04-15 11:27:28 +0530

    Home page Implemented

--- src/Components/Meals/MealItemForm.jsx
+++ src/Components/Meals/MealItemForm.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./MealItemForm.module.css";
 import OrderChange from "../UI/OrderChange";
 
 const MealItemForm = (props) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    setIsClicked(true);
+  };
+
+  const getAmount = (amount) => {
+    if (amount <= 0) {
+      setIsClicked(false);
+    }
+  };
+
   return (
     <form className={classes.form}>
-      <OrderChange />
-      <button className={classes.addToCartBtn}>+ Add</button>
+      {isClicked ? (
+        <OrderChange getAmount={getAmount} />
+      ) : (
+        <button onClick={onClickHandler} className={classes.addToCartBtn}>
+          + Add
+        </button>
+      )}
     </form>
   );
 };
--- src/Components/UI/OrderChange.jsx
+++ src/Components/UI/OrderChange.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./OrderChange.module.css";
 
-const OrderChange = () => {
+const OrderChange = (props) => {
+  const [amount, setAmount] = useState(1);
+
+  const decClickHandler = (e) => {
+    e.preventDefault();
+    setAmount(amount - 1);
+    props.getAmount(amount - 1);
+  };
+
+  const incClickHandler = (e) => {
+    e.preventDefault();
+    setAmount(amount + 1);
+    props.getAmount(amount + 1);
+  };
+
   return (
-    <div className={classes.orderChangeContainer}>
-      <button className={classes.decBtn}>-</button>
-      <p className={classes.amount}>1</p>
-      <button className={classes.incBtn}>+</button>
+    <div className={classes.orderChangeContaine}>
+      <button onClick={decClickHandler} className={classes.decBtn}>
+        -
+      </button>
+      <p className={classes.amount}>{amount}</p>
+      <button onClick={incClickHandler} className={classes.incBtn}>
+        +
+      </button>
     </div>
   );
 };
